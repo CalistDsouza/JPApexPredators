81a217626824eb62e07df18895e00068b9559503
CalistDsouza <dsouzacalist2001@gmail.com>
2025-12-23 15:57:57 -0500

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Calist Dsouza on 2025-12-23.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
