49df9a56a80d4d980342b0d1ef0f7514615fe493
CalistDsouza <dsouzacalist2001@gmail.com>
2025-12-24 00:56:00 -0500

    initial commit, adding assets

---
+++ JPApexPredators/ApexPredator.swift
@@ -0,0 +1,49 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators
+//
+//  Created by Calist Dsouza on 2025-12-23.
+// 
+
+import Foundation
+import SwiftUI
+
+struct ApexPredator : Decodable, Identifiable {
+    let id : Int
+    let name : String
+    let type : APType
+    let latitude : Double
+    let longitude : Double
+    let movies : [String]
+    let movieScenes : [MovieScenes]
+    let link : String
+    
+    var image : String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
+    
+    struct MovieScenes : Decodable {
+        let id : Int
+        let movie : String
+        let sceneDescription : String
+        
+    }
+    
+    enum APType : String, Decodable {
+        case land
+        case air
+        case sea
+        
+        var background: Color {
+            switch self {
+            case .land:
+                    .brown
+            case .air:
+                    .teal
+            case .sea:
+                    .blue
+            }
+        }
+    }
+}
